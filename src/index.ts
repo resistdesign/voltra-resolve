2f00278c837adf077c8d1ccd56c3ae9d4ee060b4
Ryanne Graff <ryantgraff@resistdesign.com>
2023-08-31 13:19:21 -0400

    chore: (repo) Automatic commit.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
-export type PathToken = {
-    [key: string]: PathToken;
+export type DependencyPathArray = (string | number)[];
+
+export type DependencyPathString = string | number;
+
+export type DependencyPath = DependencyPathArray | DependencyPathString;
+
+export type DependencyReferenceMap = Record<string, DependencyPath>;
+
+export type Dependency = {
+  dependencies?: DependencyReferenceMap;
+  getters?: DependencyReferenceMap;
+  setters?: DependencyReferenceMap;
+  factory: (dependencies: Record<string, any>) => any;
 };
 
-export type Feature<DependencyConfig extends Record<string, PathToken>, ResolvedDependenciesType extends Record<keyof DependencyConfig, any>> = {
-    dependencies?: Partial<DependencyConfig>;
-    getters?: Partial<DependencyConfig>;
-    setters?: Partial<DependencyConfig>;
-    factory: (resolvedDependencies: ResolvedDependenciesType) => Promise<any>;
-};
+export type Declaration = Dependency | Module;
 
 export type Module = {
-    [key: string]: Feature<any, any> | Module;
+  [key: string]: Declaration;
 };
+
+export const stringPathToArrayPath = (
+  path: DependencyPathString,
+): DependencyPathArray =>
+  `${path}`.split("/").map((p) => decodeURIComponent(p));
+
+export const arrayPathToStringPath = (path: DependencyPathArray): string =>
+  path.map((p) => encodeURIComponent(`${p}`)).join("/");
+
+export const getDependencyPathArray = (
+  path: DependencyPath,
+): DependencyPathArray =>
+  Array.isArray(path) ? path : stringPathToArrayPath(path);
